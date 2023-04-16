8ae2956e301d36dab0619b23bbb2c02cea346dd7
verster <rrverster31415@gmail.com>
2023-04-16 18:15:12 +0200

    feature: added button styling and animations

--- components/Sidemenu.tsx
+++ components/Sidemenu.tsx
@@ -4,6 +4,7 @@ import { ISidemenuProps, Pages } from '../types';
 import { formatEther } from 'ethers/lib/utils';
 
 import { Mainnet, DAppProvider, useEtherBalance, useEthers, Config, Goerli } from '@usedapp/core';
+import { GiFoxHead } from 'react-icons/gi';
 
 import { config } from '../types';
 const Sidemenu: React.FC<ISidemenuProps> = ({ sidemenuVisibility, toggleSideMenu, setPage }) => {
@@ -18,16 +19,6 @@ const Sidemenu: React.FC<ISidemenuProps> = ({ sidemenuVisibility, toggleSideMenu
   }
 
   const { deactivate, activateBrowserWallet } = useEthers();
-  const ConnectButton = () => {
-    // 'account' being undefined means that we are not connected.
-    if (account)
-      return (
-        <button className='bg-red-400 rounded w-full ' onClick={() => deactivate()}>
-          Disconnect
-        </button>
-      );
-    else return <button onClick={() => activateBrowserWallet()}>Connect</button>;
-  };
 
   return (
     <div
@@ -37,21 +28,26 @@ const Sidemenu: React.FC<ISidemenuProps> = ({ sidemenuVisibility, toggleSideMenu
     >
       <div className='relative h-full'>
         <div className='flex flex-col justify-center text-center'>
-          <div className=' text-white flex justify-between'>
-            <h2 className=' mb-4 text-xl'>Menu</h2>
-            <button className='' onClick={() => handleToggleClick()}>
-              <VscClose />
+          <div className='mb-4 text-white flex justify-between'>
+            <h2 className='text-xl'>Menu</h2>
+            <button onClick={() => handleToggleClick()}>
+              <VscClose size={24} />
             </button>
           </div>
-          <button className='bg-green-600 w-full p-2 my-2' onClick={() => setPage(Pages.DASHBOARD)}>
+          <button
+            className='bg-blue-400 hover:bg-blue-700 transition-all ease-in-out duration-200 hover:scale-105 hover:text-white w-full p-2 my-2 rounded'
+            onClick={() => setPage(Pages.DASHBOARD)}
+          >
             Dashboard
           </button>
-          <button className='bg-green-600 w-full p-2 my-2' onClick={() => setPage(Pages.MINTER)}>
+          <button
+            className='bg-blue-400 hover:bg-blue-700 transition-all ease-in-out duration-200 hover:scale-105 hover:text-white w-full p-2 my-2 rounded'
+            onClick={() => setPage(Pages.MINTER)}
+          >
             Minter
           </button>
 
           <div className='absolute bottom-2 w-full'>
-            <ConnectButton />
             {etherBalance && (
               <div className='bg-white text-black p-2'>
                 <br />
@@ -62,6 +58,21 @@ const Sidemenu: React.FC<ISidemenuProps> = ({ sidemenuVisibility, toggleSideMenu
                 <p className='bold'>{formatEther(etherBalance)}</p>
               </div>
             )}
+
+            <div className='mt-2 '>
+              {account ? (
+                <button className='text-white p-1 bg-red-700 w-full rounded' onClick={() => deactivate()}>
+                  Disconnect Wallet
+                </button>
+              ) : (
+                <button
+                  className='flex text-center align-middle justify-center bg-white w-full rounded p-2'
+                  onClick={() => activateBrowserWallet()}
+                >
+                  <GiFoxHead className='mr-2' size={24} /> Connect Wallet
+                </button>
+              )}
+            </div>
           </div>
         </div>
       </div>
